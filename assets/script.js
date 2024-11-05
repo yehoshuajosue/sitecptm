2f072668f2efc1750df36238381bf31d1ea3f624
Josué G. <163170107+yehoshuajosue@users.noreply.github.com>
2024-11-05 09:13:45 -0300

    Delete assets directory

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-ocument.getElementById('menu-toggle').addEventListener('click', function() {
-    const navMenu = document.querySelector('.nav-menu');
-    navMenu.classList.toggle('active');
-});
-
-
